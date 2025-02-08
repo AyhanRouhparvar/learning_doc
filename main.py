5d85ff4a30c48a74044ae57a41559954e994565c
ayhan <ayhanrouhparvar@gmail.com>
2025-02-08 09:01:03 -0800

    create interpolation

---
+++ main.py
@@ -0,0 +1,39 @@
+# interpolation
+
+
+print(f"multply 2 and 6 is : {2 * 6}")
+
+  
+myWeight = 34
+
+print(myWeight)
+
+myWeight = int(myWeight)
+
+print(myWeight)
+
+
+myWeight = float(myWeight) # conver to float
+
+print(type(myWeight)) # integer 
+
+myWeightFloat = float(myWeight) # float 
+
+print(type(myWeightFloat)) 
+
+print("-------------------------------")
+
+testNumber = "600"
+
+print(type(testNumber))
+
+testNumber = int(testNumber)
+
+print(testNumber)
+
+
+
+
+
+
+
