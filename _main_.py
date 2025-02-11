8e23fbcbd556a9e8fb139ff71605ed455fa8fa53
ayhan <ayhanrouhparvar@gmail.com>
2025-02-11 10:18:49 -0800

    -main-

---
+++ _main_.py
@@ -0,0 +1,29 @@
+# comparison operators =< return boolean
+
+number_1 = 1
+number_2 = 2
+
+# == : returns true if the value of number_1 is equal to number_2 
+
+print(f'{number_1} == {number_2} : {number_1 == number_2}')
+
+
+# != : returns true if the value of number_1 is not equal to number_2 
+  
+print(f'{number_1} != {number_2} : {number_1 != number_2}')
+
+
+# >  :1  returns true if the value of number_1 is greater than number_2 
+
+print(f'{number_1} > {number_2} : {number_1 > number_2}')
+
+
+# >=  : = or >
+
+print(f'{number_1} >= {number_2} : {number_1 >= number_2}')
+
+
+# <  :  returns true if the value of number_1 is less than number_2
+
+print(f'{number_1} < {number_2} : {number_1 < number_2}')
+
