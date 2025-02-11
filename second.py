20b59f613a8203597795866ed7589de6b878a748
ayhan <ayhanrouhparvar@gmail.com>
2025-02-11 10:19:29 -0800

    second

---
+++ second.py
@@ -0,0 +1,15 @@
+if "":
+    print('this is true')
+else:
+    print('this is false')
+     
+     
+print('enter your favorite')
+
+car = input()
+      
+if car:
+    print(f"your favorite {car}")        
+    
+    
+# falsyness =< empty objects , zero or 0 , empty string , none
