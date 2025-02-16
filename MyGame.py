c0597ceb7cee1957d636fc852f812533ca1b8997
ayhan <ayhanrouhparvar@gmail.com>
2025-02-16 09:35:19 -0800

    My Game

--- MyGame.py
+++ MyGame.py
@@ -5,18 +5,40 @@ print("Scissors...")
 player_1 = input("player_1 , make your move : ")
 player_2 = input("player_2 , make your move : ")
 
-
-if player_1 == "Rock" and player_2 == "Scissors":
-    print("player_1 wins!....")
-elif player_1 == "Rock" and player_2 == "Paper":
-    print("player_2 wins!....")
-elif player_1 == "Paper" and player_2 == "Scissors": 
-       
-   
-   
-    
-elif player_1 == player_2:
-    print("thats a tie ...")
+if player_1 == player_2:
+    print("that a tie") 
+elif player_1 == "Rock":
+    if player_2 == "Scissors":
+        print("player_1 wins!....")
+    elif player_2 == "Paper":
+        print("player_2 wins!....")
+elif player_1 == "Paper":
+    if player_2 == "Rock":
+        print("player_1 wins!....")
+    elif player_2 == "Scissors":
+        print("player_2 wins!....")
+elif player_1 == "Scissors":
+    if player_2 == "Paper":
+        print("player_1 wins!....")
+    elif player_2 == "Rock":
+        print("player_2 wins!....")        
 else:
-    print("something ")    
+    print("somthing went wrong ....")  
     
+       
+# if player_1 == "Rock" and player_2 == "Scissors":
+#     print("player_1 wins!....")
+# elif player_1 == "Rock" and player_2 == "Paper":
+#     print("player_2 wins!....")    
+# elif player_1 == "Paper" and player_2 == "Rock":
+#     print("player_1 wins!....")
+# elif player_1 == "Paper" and player_2 == "Scissors":
+#     print("player_2 wins!....")    
+# elif player_1 == "Scissors" and player_2 == "Paper":
+#     print("player_1 wins!....")
+# elif player_1 == "Scissors" and player_2 == "Rock":   
+#     print("player_2 wins!....")  
+# elif player_1 == player_2:
+#     print("that a tie") 
+# else:
+#     print("somthing went wrong ....")    
