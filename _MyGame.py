1615de66aa027e2a17afcd4b186a25e53fd42e1f
ayhan <ayhanrouhparvar@gmail.com>
2025-02-16 09:34:58 -0800

    _my game

---
+++ _MyGame.py
@@ -0,0 +1,22 @@
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player_1 = input("player_1 , make your move : ")
+player_2 = input("player_2 , make your move : ")
+
+
+if player_1 == "Rock" and player_2 == "Scissors":
+    print("player_1 wins!....")
+elif player_1 == "Rock" and player_2 == "Paper":
+    print("player_2 wins!....")
+elif player_1 == "Paper" and player_2 == "Scissors": 
+       
+   
+   
+    
+elif player_1 == player_2:
+    print("thats a tie ...")
+else:
+    print("something ")    
+    
