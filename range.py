c123ffd566307914862904363c2c22fc596b287e
ayhan <ayhanrouhparvar@gmail.com>
2025-02-18 11:56:33 -0800

    Range

---
+++ range.py
@@ -0,0 +1,9 @@
+# range() => to create a list of numbers
+# 1 , 2 , 3 , ... , 20
+
+
+print(list(range(1, 10))) # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(10))) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(1, 15, 2))) # [1, 3, 5, 7, 9, 11, 13]
+print(list(range(10, 1, -1))) # [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
