b2047bdcd3d2642f3c26d5fe6ad7d351285f139c
ayhan <ayhanrouhparvar@gmail.com>
2025-02-22 08:02:58 -0800

    list_1

---
+++ list.py
@@ -0,0 +1,21 @@
+# list
+
+item_1 = "python"
+item_2 = "ionic"
+item_3 = "kotlin"
+item_4 = "JQuery"
+
+myList = [item_1, item_2, item_3, item_4]
+
+print(len(myList))
+
+# print(myList[len(myList) -1])
+# print(myList[-4])
+
+
+# myRange = range(30)
+# print(list(myRange))
+
+isExisKotlin = "kotlin" in myList
+
+print(isExisKotlin)
