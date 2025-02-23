cc026645db8a949a0446257d84dcbdf4a943ae21
ayhan <ayhanrouhparvar@gmail.com>
2025-02-23 12:22:26 -0800

    list_3

--- list-functions.py
+++ list-functions.py
@@ -1,3 +1,18 @@
 # list functions
 
-myCourser = "python"
+myCourser = ["Python", "Kotlin", "Ionic"]
+
+print(myCourser)
+
+# myCourser_1 = ["JQuery", "Vue Js"]
+
+# myCourser.append(myCourser_1)
+
+# myCourser.append(myCourser_1)
+
+# myCourser.extend(["JQuery", "Vue Js"])
+
+myCourser.insert(-1, "JQuery")
+
+print(myCourser)
+print(f"number of list items is : {len(myCourser)}")
