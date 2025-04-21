f5a93eda00557fa237960d26deb27563282a7987
ayhan <ayhanrouhparvar@gmail.com>
2025-04-21 21:25:56 +0330

    function

---
+++ function.py
@@ -0,0 +1,21 @@
+# what is a function
+
+# number = [1, 2, 3, 4, 5]
+
+# number.append(9)
+
+# print(number)
+
+
+def nameOfFunction ():
+    command
+    
+def sayHello():
+    print("hello")
+    print("------------------------")    
+
+
+sayHello()
+sayHello()    
+sayHello()    
+sayHello() 
